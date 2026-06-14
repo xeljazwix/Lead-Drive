4e89b4d6d125c752172098b585b8940c44959142
xEljazwi <133246099+xEljazwi@users.noreply.github.com>
2026-06-14 15:07:19 +0200

    Clean up: remove uploads and test files from tracking

--- test2.js
+++
@@ -1,3 +0,0 @@
-import { ZipArchive } from 'archiver';
-const archive = new ZipArchive({ zlib: { level: 4 } });
-console.log('ZipArchive instantiated:', typeof archive.file, typeof archive.finalize);
